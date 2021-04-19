39de153ca3488ce0b0e796b590c6342cf421f536
wangqingyu-dev <1096036749@qq.com>
2021-04-19 16:31:35 +0800

    分支测试

--- music-website/music-server/src/main/java/com/example/demo/HwMusicApplication.java
+++ music-website/music-server/src/main/java/com/example/demo/HwMusicApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HwMusicApplication {
 
     public static void main(String[] args) {
+        System.out.println("成功");
         SpringApplication.run(HwMusicApplication.class, args);
     }
 
